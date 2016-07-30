accd5e4115171d1e82e9387a5b07b3a5efbaa7f0
Evan Dixon <evanldixon@live.com>
2016-07-30 13:02:19 -0500

    Initial commit

---
+++ ClickOnce-Deployment-Generator/Module1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Reflection
+
+Module Module1
+
+    Sub Main()
+        Dim options As New Options
+        If CommandLine.Parser.Default.ParseArguments(Environment.GetCommandLineArgs, options) Then
+            Dim exeAssembly As Assembly = Assembly.ReflectionOnlyLoadFrom(options.Executable)
+            Dim version = exeAssembly.GetName.Version.ToString
+
+            'If the executable is in bin\Release\Something.exe, we want the working directory to be bin
+            Dim originalSourceDirectory = IO.Path.GetFileName(IO.Path.GetDirectoryName(options.Executable)) 'Should be Release in this example
+            Environment.CurrentDirectory = IO.Path.Combine(IO.Path.GetDirectoryName(options.Executable), "..")
+
+            'Copy the source files (e.g. from bin\Release to bin\Deploy\1.0.0.0)
+            Dim newSourceDirectory = IO.Path.Combine("Deploy", version)
+            If IO.Directory.Exists("Deploy") Then
+                IO.Directory.Delete("Deploy")
+            End If
+            IO.Directory.CreateDirectory("Deploy")
+            My.Computer.FileSystem.CopyDirectory(originalSourceDirectory, newSourceDirectory)
+
+            Dim manifestName As String = IO.Path.Combine("Deploy", version, IO.Path.GetFileName(options.Executable) & ".manifest")
+            Dim deploymentName As String = IO.Path.Combine("Deploy", IO.Path.GetFileName(options.Executable) & ".application")
+
+            'Create the manifest
+            RunProgram(options.MageFilename, $"-New Application -Processor ""{options.Architecture}"" -ToFile ""{manifestName}"" -version ""{version}"" -name ""{exeAssembly.GetName.Name}"" -FromDirectory ""{newSourceDirectory}""")
+
+            'Todo: enable extension mapping
+            'Todo: rename all files but the manifest to "*.deploy"
+
+            'Sign the manifest
+            RunProgram(options.MageFilename, $"-Sign ""{manifestName}"" -CertFile ""{options.KeyFilename}"" -Password {options.KeyPassword}")
+
+            'Create the deployment (.application)
+            RunProgram(options.MageFilename, $"-New Deployment -Processor ""{options.Architecture}"" -Install true -Publisher ""{options.Publisher}"" -version ""{version}"" -ProviderUrl ""{options.ProviderUrl}"" -AppManifest ""{manifestName}"" -UseManifestForTruest true -ToFile ""{deploymentName}""")
+
+            'Sign the deployment
+            RunProgram(options.MageFilename, $"-Sign ""{deploymentName}"" -CertFile ""{options.KeyFilename}"" -Password {options.KeyPassword}")
+        End If
+    End Sub
+
+    Sub RunProgram(program As String, arguments As String)
+        Dim p As New Process
+        p.StartInfo.WorkingDirectory = Environment.CurrentDirectory
+        p.StartInfo.FileName = program
+        p.StartInfo.Arguments = arguments
+        p.Start()
+        p.WaitForExit()
+    End Sub
+
+End Module
---
+++ ClickOnce-Deployment-Generator/Options.vb
@@ -0,0 +1,31 @@
+﻿Imports CommandLine
+
+Public Class Options
+
+    <[Option]("e"c, "Executable", HelpText:="The main executable of the application.  Should be in its own directory, with any other files to be included in the same deployment.", Required:=True)>
+    Public Property Executable As String
+
+    <[Option]("u"c, "ProviderUrl", HelpText:="The URI the application file is stored.", Required:=True)>
+    Public Property ProviderUrl As String
+
+    <[Option]("p"c, "Publisher", HelpText:="Name of the publisher of the deployment.", Required:=True)>
+    Public Property Publisher As String
+
+    <[Option]("a"c, "Architecture", HelpText:="Architecture of the processor.  Valid values: ""msil"", ""x86"", and ""x64"".", Required:=True)>
+    Public Property Architecture As String
+
+    <[Option]("KeyFilename", HelpText:="Filename of the key used for signing.", Required:=True)>
+    Public Property KeyFilename As String
+
+    <[Option]("KeyPassword", HelpText:="Password of the key used for signing.", Required:=True)>
+    Public Property KeyPassword As String
+
+    <[Option]("MageFilename", HelpText:="Path of mage.exe", Required:=True)>
+    Public Property MageFilename As String
+
+    <HelpOption>
+    Public Function GetUsage() As String
+        Return "Usage documentation not added yet :("
+    End Function
+
+End Class
